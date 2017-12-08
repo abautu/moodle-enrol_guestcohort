ed66d0afb52e5c2e9462693141feccd8e7a5b2fb
Andrei Bautu <abautu@gmail.com>
2017-12-08 08:56:43 +0200

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017120600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017120800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016052300;        // Requires this Moodle version.
 $plugin->release   = '1.0beta1';
 $plugin->component = 'enrol_guestcohort';    // Full name of the plugin (used for diagnostics).
